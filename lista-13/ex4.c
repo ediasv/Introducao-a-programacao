21261ead26be077fbb8d7546424bcccce2ed6add
Eduardo Dias <eduardoviniciusdias@alunos.utfpr.edu.br>
2023-10-14 00:33:43 -0300

    resolvendo exercicios

---
+++ lista-13/ex4.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+#define N_MAX 100
+
+int main()
+{
+    int i, n, secoes[N_MAX], soma, acha_k;
+
+    scanf("%d", &n);
+
+    soma = 0;
+    for (i = 0; i < n; i++ )
+    {
+        scanf("%d", &secoes[i]);
+        soma += secoes[i];
+    }
+    acha_k = soma / 2;
+    soma = 0;
+    for (i = 0; i < n; i++ )
+    {
+        soma += secoes[i];
+        if (soma == acha_k)
+        {
+            break;
+        }
+    }
+    
+    printf("%d\n", i+1);
+    return 0;
+}
---
+++ lista-13/ex5.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+#define N_MAX 50
+
+int main()
+{
+    int i, n, minas[N_MAX], tabuleiro[N_MAX];
+
+    scanf("%d", &n);
+
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &minas[i]);
+        tabuleiro[i] = 0;
+    }
+
+    if (minas[0] == 1)
+    {
+        tabuleiro[0]++;
+        tabuleiro[1]++;
+    }
+
+    if (minas[n-1] == 1)
+    {
+        tabuleiro[n-1]++;
+        tabuleiro[n-2]++;
+    }
+
+    for (i = 1; i < n-1; i++)
+    {
+        if (minas[i]==1)
+        {
+            tabuleiro[i-1]++;
+            tabuleiro[i]++;
+            tabuleiro[i+1]++;
+        }
+    }
+    
+    for (i = 0; i < n; i++)
+    {
+        printf("%d ", tabuleiro[i]);
+    }
+    printf("\n");
+    return 0;
+}
